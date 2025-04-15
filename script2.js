59dc8020da810608a594fe6198d3bce8c14388df
Jayden Mclin <101745138+jaydenmcl@users.noreply.github.com>
2025-04-15 10:32:44 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,2 +1,10 @@
-pnckey = "`" //Key used for quickly replacing the tab with a school-safe oneq
+pnckey = "=" //Key used for quickly replacing the tab with a school-safe oneq
 
+document.onkeydown=(e)=>{
+	console.log('Keydown');
+	switch(e.key) {
+		case pnckey:e.preventDefault();
+		e.stopPropagation();
+		replace('https://classroom.google.com/h');
+	}
+}
